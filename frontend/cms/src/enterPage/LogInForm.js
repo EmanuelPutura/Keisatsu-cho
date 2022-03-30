dd67cda73cfb910699b282691f4687fb5d7d9e80
916-Rusu-Horia-Radu <72196149+HoriaRaduRusu@users.noreply.github.com>
2022-03-30 17:33:40 +0300

    changed to functional components

--- frontend/cms/src/enterPage/LogInForm.js
+++ frontend/cms/src/enterPage/LogInForm.js
@@ -1,51 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./form.css"
 
-class LogInForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        };
+function LogInForm(){
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        this.setState({[name]: value});
-    }
-
-
-    handleSubmit(event) {
-        alert('Submitted ' + this.state.email + " " + this.state.password);
+    function handleSubmit(event) {
+        alert('Submitted ' + email + " " + password);
         event.preventDefault();
     }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} className="form">
-                <h2>LOG IN</h2>
-                <label>E-mail:
-                    <br/>
+    return (
+        <form onSubmit={handleSubmit} className="form">
+            <h2>LOG IN</h2>
+            <label>E-mail:
+                <br/>
                 <input name="email" type="text"
-                       value={this.state.email}
-                       onChange={this.handleInputChange}/>
-                </label>
-                <label>Password:
-                    <br/>
+                       value={email}
+                       onChange={e => setEmail(e.target.value)}/>
+            </label>
+            <label>Password:
+                <br/>
                 <input name="password" type="password"
-                       value={this.state.password}
-                       onChange={this.handleInputChange}/>
-                </label>
-                <input type="submit" value="Log in"/>
-            </form>
-        )
-    }
+                       value={password}
+                       onChange={e => setPassword(e.target.value)}/>
+            </label>
+            <input type="submit" value="Log in"/>
+        </form>
+    )
 }
 
 export default LogInForm
--- frontend/cms/src/enterPage/SignUpForm.js
+++ frontend/cms/src/enterPage/SignUpForm.js
@@ -1,84 +1,68 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./form.css"
 
-class SignUpForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            user_name: '',
-            password: '',
-            password_repeat: '',
-            user_type: '',
-        };
+function SignUpForm(){
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        this.setState({[name]: value});
-    }
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [user_name, setUser_name] = useState("");
+    const [password_repeat, setPassword_repeat] = useState("");
+    const [user_type, setUser_type] = useState("");
 
 
-    handleSubmit(event) {
-        alert('Submitted ' + this.state.email + ' ' + this.state.user_name + ' ' + this.state.password + ' ' + this.state.user_type);
+    function handleSubmit(event) {
+        alert('Submitted ' + email + ' ' + user_name + ' ' + password + ' ' + user_type);
         event.preventDefault();
     }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} className="form">
-                <h2>SIGN UP</h2>
-                <label>E-mail:
-                    <br/>
-                    <input name="email" type="text"
-                           value={this.state.email}
-                           onChange={this.handleInputChange}/>
-                </label>
-                <label>Name:
-                    <br/>
-                    <input name="user_name" type="text"
-                           value={this.state.user_name}
-                           onChange={this.handleInputChange}/>
-                </label>
-                <label>User Type:</label>
-                <div className="type_container">
-                    <label>
+    return (
+        <form onSubmit={handleSubmit} className="form">
+            <h2>SIGN UP</h2>
+            <label>E-mail:
+                <br/>
+                <input name="email" type="text"
+                       value={email}
+                       onChange={e => setEmail(e.target.value)}/>
+            </label>
+            <label>Name:
+                <br/>
+                <input name="user_name" type="text"
+                       value={user_name}
+                       onChange={e => setUser_name(e.target.value)}/>
+            </label>
+            <label>User Type:</label>
+            <div className="type_container">
+                <label>
                     <input value="chair" name="user_type" type="radio"
-                           checked={this.state.user_type === "chair"}
-                           onChange={this.handleInputChange}/> Chair
-                    </label>
-                    <label>
+                           checked={user_type === "chair"}
+                           onChange={e => setUser_type(e.target.value)}/> Chair
+                </label>
+                <label>
                     <input value="reviewer" name="user_type" type="radio"
-                           checked={this.state.user_type === "reviewer"}
-                           onChange={this.handleInputChange}/> Reviewer
-                    </label>
-                    <label>
+                           checked={user_type === "reviewer"}
+                           onChange={e => setUser_type(e.target.value)}/> Reviewer
+                </label>
+                <label>
                     <input value="author" name="user_type" type="radio"
-                           checked={this.state.user_type === "author"}
-                           onChange={this.handleInputChange}/> Author
-                    </label>
-                </div>
-                <label>Password:
-                    <br/>
-                    <input name="password" type="password"
-                           value={this.state.password}
-                           onChange={this.handleInputChange}/>
+                           checked={user_type === "author"}
+                           onChange={e => setUser_type(e.target.value)}/> Author
                 </label>
-                <label>Confirm password:
-                    <br/>
-                    <input name="password_repeat" type="password"
-                           value={this.state.password_repeat}
-                           onChange={this.handleInputChange}/>
-                </label>
-                <input type="submit" value="Sign up"/>
-            </form>
-        )
-    }
+            </div>
+            <label>Password:
+                <br/>
+                <input name="password" type="password"
+                       value={password}
+                       onChange={e => setPassword(e.target.value)}/>
+            </label>
+            <label>Confirm password:
+                <br/>
+                <input name="password_repeat" type="password"
+                       value={password_repeat}
+                       onChange={e => setPassword_repeat(e.target.value)}/>
+            </label>
+            <input type="submit" value="Sign up"/>
+        </form>
+    )
 }
 
 export default SignUpForm
