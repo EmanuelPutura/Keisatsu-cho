5c7558a120347752ba5e5ec13636242bab0532a3
PredaBoss <50477058+PredaBoss@users.noreply.github.com>
2022-04-30 16:19:48 +0300

    Revert "Database hosting"

--- backend/cms/build.gradle.kts
+++ backend/cms/build.gradle.kts
@@ -17,14 +17,11 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    implementation ("org.postgresql:postgresql:42.3.3")
-    runtimeOnly("org.postgresql:postgresql")
 }
 
 tasks.withType<KotlinCompile> {
