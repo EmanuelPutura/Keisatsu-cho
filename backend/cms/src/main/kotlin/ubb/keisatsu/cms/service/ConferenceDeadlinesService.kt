4afdb6fa086f13efe9f1f0c187d9c16354d54769
EmanuelPutura <emanuelputura@gmail.com>
2022-05-21 16:32:48 +0300

    Chair page deadlines validation

--- backend/cms/src/main/kotlin/ubb/keisatsu/cms/service/ConferenceDeadlinesService.kt
+++ backend/cms/src/main/kotlin/ubb/keisatsu/cms/service/ConferenceDeadlinesService.kt
@@ -1,10 +1,23 @@
 package ubb.keisatsu.cms.service
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import ubb.keisatsu.cms.model.entities.ConferenceDeadlines
 import ubb.keisatsu.cms.repository.ConferenceDeadlinesRepository
+import java.time.LocalDate
 
 @Service
 class ConferenceDeadlinesService(private val conferenceDeadlinesRepository: ConferenceDeadlinesRepository) {
+    fun retrieveConferenceDeadlines(conferenceDeadlinesId: Int): ConferenceDeadlines? = conferenceDeadlinesRepository.findByIdOrNull(conferenceDeadlinesId)
+
     fun addConferenceDeadlines(conferenceDeadlines: ConferenceDeadlines): ConferenceDeadlines = conferenceDeadlinesRepository.save(conferenceDeadlines)
+
+    fun validateDeadlines(submissionDeadline: LocalDate, reviewDeadline: LocalDate, acceptanceDeadline: LocalDate, uploadDeadline: LocalDate): Boolean {
+        return submissionDeadline.isBefore(reviewDeadline) && reviewDeadline.isBefore(acceptanceDeadline) && acceptanceDeadline.isBefore(uploadDeadline) &&
+                uploadDeadline.isBefore(LocalDate.now())
+    }
+
+    fun isDeadlineStillValid(deadline: LocalDate): Boolean {
+        return LocalDate.now().isBefore(deadline)
+    }
 }
